b01677b4961eb445cb9d409f67bf129969039f67
Olaide <olaidemmanuel1@gmail.com>
2020-03-25 11:28:30 +0100

    Started with the functionality using React Hooks for State management

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
  const Transaction = () => {
+     const [text, setText] = useState("");
+     const [amount, setAmount] = useState(0);
     return (
         <>
             <h3>Add New Transaction</h3>
             <form>
                 <div className="form-control">
                     <label htmlFor="text">Test</label>
-                    <input type="text" placeholder="Enter text here ..." />
+                    <input type="text" value={text} onChange={(e) => setText(e.target.value)}
+                     placeholder="Enter text here ..." />
                 </div>
                 <div className="form-control">
                     <label htmlFor="amount">
                         Amount <br />
                         (negative - expense, positive - income)
                     </label>
-                    <input type="number" placeholder="Enter amount here ...." />
+                    <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)}
+                    placeholder="Enter amount here ...." />
                 </div>
                 <button className="btn">Add Transaction in Naira</button>
             </form>
