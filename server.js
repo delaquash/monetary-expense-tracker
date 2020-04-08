9892bbeee193744397dfe062ba7c491016f7249b
Olaide <olaidemmanuel1@gmail.com>
2020-04-08 13:50:50 +0100

    Used MongoDB cloud for database connection

--- server.js
+++ server.js
@@ -2,14 +2,19 @@ const express = require('express');
 const dotenv = require('dotenv');
 const colors = require("colors");
 const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 
-dotenv.config({ path: './config/config.env' });
+dotenv.config({
+    path: './config/config.env'
+});
+connectDB();
 const transaction = require('./routes/transaction');
 
 
+
 const app = express();
-app.use('/api/v1/transaction', transaction);
+app.use('/api/v1/transactions', transaction);
 
 const PORT = process.env.PORT || 5000;
 
