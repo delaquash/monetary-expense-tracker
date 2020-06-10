78902d0e6038df006d6d377b28486f1c8410eea7
Olaide <olaidemmanuel1@gmail.com>
2020-06-10 20:54:18 +0100

    Ready to deploy

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const colors = require('colors');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
 
-dotenv.config({ path: './config/config.env' });
+dotenv.config({
+    path: './config/config.env'
+});
 
 connectDB();
 const transactions = require('./routes/transactions');
@@ -13,8 +15,11 @@ const app = express();
 
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 app.use('/api/v2/transactions', transactions);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
