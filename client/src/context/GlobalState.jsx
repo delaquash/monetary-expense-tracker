c37a47822db89c5c0f0f640a962c8f75f2f7a78c
Olaide Emmanuel <olaidemmanuel1@gmail.com>
2022-05-31 11:42:29 +0100

    Update GlobalState.jsx
    Fix issue in state

--- client/src/context/GlobalState.jsx
+++ client/src/context/GlobalState.jsx
@@ -1,90 +1,38 @@
 import React, { createContext, useReducer } from 'react';
 import AppReducer from './AppReducer';
-// import MainTransaction from '../components/MainTransaction';
-import axios from 'axios';
 
-// Initial State
-const InitialState = {
-  transactions: [],
-  error: null,
-  loading: true,
-};
+// Initial state
+const initialState = {
+  transactions: []
+}
 
 // Create context
-
-export const GlobalContext = createContext(InitialState);
+export const GlobalContext = createContext(initialState);
 
 // Provider component
 export const GlobalProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(AppReducer, InitialState);
+  const [state, dispatch] = useReducer(AppReducer, initialState);
 
-  // Async function for axios
-
-  async function getTransactions() {
-    try {
-      const res = await axios.get('/api/v2/transactions');
-      dispatch({
-        type: 'GET_TRANSACTIONS',
-        payload: res.data.data,
-      });
-    } catch (err) {
-      dispatch({
-        type: 'TRANSACTION_ERROR',
-        payload: err.response.data.error,
-      });
-    }
+  // Actions
+  function deleteTransaction(id) {
+    dispatch({
+      type: 'DELETE_TRANSACTION',
+      payload: id
+    });
   }
 
-  // Action for deleting an expense
-  async function deleteTransaction(id) {
-    try {
-         await axios.delete(`/api/v2/transactions/${id}`);
-                dispatch({
-                  type: 'DELETE_TRANSACTION',
-                  payload: id
-                });
-    } catch (err) {
-      dispatch({
-        type: 'TRANSACTION_ERROR',
-        payload: err.response.data.error
-      });
-    }
+  function addTransaction(transaction) {
+    dispatch({
+      type: 'ADD_TRANSACTION',
+      payload: transaction
+    });
   }
 
-
-  // Action for adding an expense
-  async function addTransaction(transaction) {
-    const config = {
-      headers: {
-        "Content-Type": "application/json"
-      }
-    };
-    try {
-      const res = await axios.post("/api/v2/transactions", transaction, config);
-        dispatch({
-        type: 'ADD_TRANSACTION',
-        payload: res.data.data
-      });
-    } catch (err) {
-        dispatch({
-        type: 'TRANSACTION_ERROR',
-        payload: err.response.data.error
-      });
-    }
-  }
-
-  return (
-    <GlobalContext.Provider
-      value={{
-        transactions: state.transactions,
-        deleteTransaction,
-        addTransaction,
-        error: state.error,
-        getTransactions,
-        loading: state.loading,
-      }}
-    >
-      {children}
-    </GlobalContext.Provider>
-  );
-};
+  return (<GlobalContext.Provider value={{
+    transactions: state.transactions,
+    deleteTransaction,
+    addTransaction
+  }}>
+    {children}
+  </GlobalContext.Provider>);
+}
